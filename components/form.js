c384617ade774a8972579170ef9cfa9e8bfcafac
Coding rev <fernando.lobo.prez@gmail.com>
2024-10-31 19:18:46 -0300

    03-Getting form values
    03-Getting form values

--- components/form.js
+++ components/form.js
@@ -1,34 +1,53 @@
 import { Text, TextInput, View, StyleSheet, Button } from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
+
 
 export default function FormComp(){
     return(
+        <>
+        <Formik
+            initialValues={{email:'',password:''}}
+            onSubmit={values=>console.log(values)}
+        >
+        { ({handleChange, handleBlur, handleSubmit, values, errors, touched })=> (
         <View>
             <TextInput
                 style={styles.inputStyle}
                 placeholder="Enter your email"
-                // onChangeText={}
-                // onBlur={}
-                value={'gmail@gmail.com'}
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
                 keyboardType="email-address"
             />
-            <Text style={styles.errorLabel}>Error</Text>
-
+            {(errors.email && touched.email) &&
+                <Text style={styles.errorLabel}>
+                    {errors.email}
+                </Text>
+            }
+            
             <TextInput
                 style={styles.inputStyle}
                 placeholder="Enter your password"
-                // onChangeText={}
-                // onBlur={}
-                value={'ssssss'}
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
                 secureTextEntry={true}
             />
-            <Text style={styles.errorLabel}>Error</Text>
+           {(errors.password && touched.password) &&
+                <Text style={styles.errorLabel}>
+                    {errors.password}
+                </Text>
+            }
 
             <Button
                 title="Submit"
-                
+                onPress={handleSubmit}
             />
-
         </View>
+        )}
+        </Formik>
+        </>
     )
 }
 
